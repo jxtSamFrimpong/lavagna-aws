e6b14a49b558675ccc09327abb95e31c8475c28f
Ewin <57272639+jxtSamFrimpong@users.noreply.github.com>
2022-11-25 06:22:31 +0000

    rds

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-java -Ddatasource.dialect="${DB_DIALECT}" -Ddatasource.url="${DB_URL}" -Ddatasource.username="${DB_USER}" -Ddatasource.password="${DB_PASS}" -Dspring.profile.active="${SPRING_PROFILE}" -jar lavagna-jetty-console.war
+java -Ddatasource.dialect="${DB_DIALECT}" -Ddatasource.url="${DB_URL}" -Ddatasource.username="${DB_USER}" -Ddatasource.password="${DB_PASS}" -jar lavagna-jetty-console.war
 # java -Xms64m -Xmx128m -Ddatasource.dialect="${DB_DIALECT}" \
 #     -Ddatasource.url="${DB_URL}" \
 #     -Ddatasource.username="${DB_USER}" \
 #     -Ddatasource.password="${DB_PASS}" \
 #     -Dspring.profiles.active="${SPRING_PROFILE}" \
 #     -jar ./lavagna/lavagna/lavagna-jetty-console.war --headless
+# -Dspring.profile.active="${SPRING_PROFILE}"
